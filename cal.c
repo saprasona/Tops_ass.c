85aa049c67fd40a5697b9033e525727e05a61b45
saprasona <128086441+saprasona@users.noreply.github.com>
2023-04-15 22:51:32 +0530

    Add files via upload

---
+++ cal.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{ 
+    int a,b,c,ch;
+
+        printf("\n1.Addition\n");
+        printf("2.Substract\n");
+        printf("3.Multiplication\n");
+        printf("4.Division\n");
+        printf("5.Modulo\n");
+        printf("6.Exit\n");
+        printf("Chosse any one option\n");
+        scanf("%d",&ch);
+        
+        printf("Enter the first number=");
+        scanf("%d",&a);
+        printf("Enter the second number=");
+        scanf("%d",&b);
+        
+        switch (ch)
+        {
+        case 1: printf("Addition=%d",c=a+b);
+                scanf("%d",&c);
+            break;
+        case 2: printf("Substract=%d",c=a-b);
+                scanf("%d",&c);
+            break;
+        case 3: printf("Multiplication=%d",c=a*b);
+                scanf("%d",&c);
+            break;
+        case 4: printf("Division=%d",c=a/b);
+                scanf("%c",&c);
+            break;
+        case 5: printf("Modulo=%d",c=a%b);
+                scanf("%d",&c);
+            break;
+        case 6: printf("Exit");
+        break;                     
+        default: printf("Enter Valid input\n");
+            break;
+        }
+    
+    return 0;
+}
